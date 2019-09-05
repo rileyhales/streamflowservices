99d8767d1d73909ea84412ef1241c57c28c30def
rileyhales <rileyhales1@gmail.com>
2019-09-05 17:14:08 -0600

    add south america and asia to the app

--- tethysapp/streamflowservices/options.py
+++ tethysapp/streamflowservices/options.py
@@ -2,6 +2,7 @@ def watersheds_db():
     return (
         # ('North America', 'north_america-geoglows'),
         ('Central America', 'central_america-geoglows'),
-        # ('South America', 'south_america-geoglows'),
+        ('South America', 'south_america-geoglows'),
         ('Africa', 'africa-geoglows'),
+        ('South Asia', 'south_asia-geoglows'),
     )
