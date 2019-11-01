e70b4acea48cc0b5a2e5efa661129f3941e67f9f
rileyhales <rileyhales1@gmail.com>
2019-11-01 11:08:12 -0600

    add 2 regions

--- tethysapp/streamflowservices/options.py
+++ tethysapp/streamflowservices/options.py
@@ -1,6 +1,6 @@
 def watersheds_db():
     return (
-        # ('Indonesia', 'indonesia-geoglows'),
+        ('Indonesia', 'indonesia-geoglows'),
         ('Australia', 'australia-geoglows'),
         ('Japan', 'japan-geoglows'),
         ('East Asia', 'east_asia-geoglows'),
@@ -8,7 +8,7 @@ def watersheds_db():
         ('Central Asia', 'central_asia-geoglows'),
         # ('West Asia', 'west_asia-geoglows'),
         ('Middle East', 'middle_east-geoglows'),
-        # ('Europe', 'europe-geoglows'),
+        ('Europe', 'europe-geoglows'),
         ('Africa', 'africa-geoglows'),
         ('South America', 'south_america-geoglows'),
         ('Central America', 'central_america-geoglows'),
