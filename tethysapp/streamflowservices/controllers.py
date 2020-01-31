cc9ccfa31c1cc9af0c20621f2061508f2df058cd
rileyhales <rileyhales1@gmail.com>
2020-01-31 14:42:18 -0700

    add new regions

--- tethysapp/streamflowservices/controllers.py
+++ tethysapp/streamflowservices/controllers.py
@@ -9,19 +9,19 @@ from tethys_sdk.gizmos import SelectInput
 from .app import Streamflowservices as App
 
 watersheds_db = (
-        ('Indonesia', 'indonesia-geoglows'),
+        ('Islands', 'islands-geoglows'),
         ('Australia', 'australia-geoglows'),
         ('Japan', 'japan-geoglows'),
         ('East Asia', 'east_asia-geoglows'),
         ('South Asia', 'south_asia-geoglows'),
         ('Central Asia', 'central_asia-geoglows'),
-        # ('West Asia', 'west_asia-geoglows'),
+        ('West Asia', 'west_asia-geoglows'),
         ('Middle East', 'middle_east-geoglows'),
         ('Europe', 'europe-geoglows'),
         ('Africa', 'africa-geoglows'),
         ('South America', 'south_america-geoglows'),
         ('Central America', 'central_america-geoglows'),
-        # ('North America', 'north_america-geoglows')
+        ('North America', 'north_america-geoglows')
 )
 
 
